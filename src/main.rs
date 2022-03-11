1678b4598a8a29921542a3acc118350d5224728a
lei <wanglei4687@gmail.com>
2022-03-12 00:00:01 +0800

    add sentry

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    
+    let _guard = sentry::init(("https://2fb0e3d203c2413b9b20305ef37ccd97@o1034490.ingest.sentry.io/6254733", sentry::ClientOptions {
+        release: sentry::release_name!(),
+        ..Default::default()
+    }));
+
     println!("Hello, world!");
 }
